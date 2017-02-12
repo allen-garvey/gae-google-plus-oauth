f89cbccf962376bfbf883eaa6fa16b5c8b5c1fe1
Allen Garvey <allen.garvey@gmail.com>
2017-02-11 19:19:37 -0500

    Add import for secret

--- controllers/oauth_controller.py
+++ controllers/oauth_controller.py
@@ -1,10 +1,12 @@
 import webapp2
 from controllers.base_controller import BaseController
+from secret import oauth_secret
 
 class OauthController(BaseController):
 	"""Retrieves data from Google after they are redirected back
 	after Oauth login"""
 
 	def get(self):
+		secrets = oauth_secret()
 		self.response.write("hello from oauth")
 		
