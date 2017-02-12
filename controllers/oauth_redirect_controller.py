a15f24e07411086f0ea15c6c84e7d08eaedb57ec
Allen Garvey <allen.garvey@gmail.com>
2017-02-11 19:19:49 -0500

    Change redirect to link to redirect

--- controllers/oauth_redirect_controller.py
+++ controllers/oauth_redirect_controller.py
@@ -9,5 +9,8 @@ class OauthRedirectController(BaseController):
 	def get(self):
 		params_dict = oauth_secret()
 		google_oauth_url = "https://accounts.google.com/o/oauth2/v2/auth"
-		self.redirect(google_oauth_url + self.to_query_string(params_dict))
+		redirect_url = google_oauth_url + self.to_query_string(params_dict)
+		#redirect changed to link to prevent site being categorized as phishing
+		# self.redirect(redirect_url)
+		self.response.write("<h1>Google Oauth Implementation</h1><a href='" + redirect_url + "'>By clicking this link, you agree to share your Google+ email address and name</a>")
 		
